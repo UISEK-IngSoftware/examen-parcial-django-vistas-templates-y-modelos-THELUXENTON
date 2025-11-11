18bf1694d29480c7fd76d4776eea27b8ea709072
Sebastian <p@MacBook-Air-de-Sebastian.local>
2025-11-11 09:31:08 -0500

    Examen Parcial

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+  
+    title = models.CharField(max_length=200) 
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    synopsis = models.TextField()
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Movie
+from django.http import HttpResponse 
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies_list': movies
+    }
+    return render(request, 'movies/index.html', context)
+
+
+def movie_detail(request, movie_id):
+    try:
+        movie = Movie.objects.get(pk=movie_id)
+        
+        context = {
+            'movie': movie 
+        }
+        return render(request, 'movies/movie_detail.html', context)
+        
+    except Movie.DoesNotExist:
+     
+        return HttpResponse("<h1>Error: Película no encontrada</h1>", status=404)
